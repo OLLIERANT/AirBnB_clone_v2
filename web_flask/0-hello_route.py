58100d2731b32f776510dd226ee20a51c35e03ff
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2024-01-30 03:18:42 -0800

    web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Script that starts a Flask web application """
 
 from flask import Flask
 
@@ -7,6 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """print web"""
     return 'Hello HBNB!'
 
 
