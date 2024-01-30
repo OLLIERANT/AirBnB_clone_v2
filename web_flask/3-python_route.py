2fd532ee0499aec3feef0bf5fe8864883fa966d9
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2024-01-30 04:15:20 -0800

    python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,6 +25,7 @@ def c_with_text(text):
     return "C {}".format(formatted_text)
 
 
+# Define the route for '/python/<text>
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_with_text(text):
