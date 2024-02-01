9383d82d60fb08eab46fff10643641e344d2cfe6
Babalola Olaoluwa <lalaboiydvibeplug@gmail.com>
2024-02-01 03:28:31 -0800

    odd or even route

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """print web"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_with_text(text):
+    """print C followed by the value of the text variable"""
+    formatted_text = text.replace('_', ' ')
+    return "C {}".format(formatted_text)
+
+
+# Define the route for '/python/<text>
+@app.route('/python', defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/python/', defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_with_text(text):
+    """print python followed by the value of the text variable"""
+    formatted_text = text.replace('_', ' ')
+    return "Python {}".format(formatted_text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+
+# Define the route for '/number_templates/<n>'
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Displays an HTML page only if <n> is an integer"""
+    # Render the template and pass the value of n to the template
+    return render_template("5-number.html", n=n)
+
+
+# Define the route for '/number_odd_or_even/<n>'
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """Displays an HTML page only if <n> is even or odd"""
+    # Render the template and pass the value of n to the template
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
